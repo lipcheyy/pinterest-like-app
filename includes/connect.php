23e5100d4cfb997b8301157300a2ad77d8da0dfb
vlad <vladlipchey05@gmail.com>
2022-10-29 20:29:16 +0300

    feat:add some styling for forms

--- includes/connect.php
+++ includes/connect.php
@@ -1,5 +1,5 @@
 <?php
-$connect= mysqli_connect('localhost','root','','testing-app');
+$connect= mysqli_connect('localhost','root','','gallery');
 //debug for chercking if db connected
 if (!$connect){
     die('disconnected');
