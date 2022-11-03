c9423fbc0ebcf66c6e4645fb650c1aa581686b08
Vladislav <vladlipchey05@gmail.com>
2022-11-04 00:06:24 +0200

    change display of information in admin panel

--- includes/admin.php
+++ includes/admin.php
@@ -1,20 +1,11 @@
 <?php
 require_once "connect.php";
-$query=mysqli_query($connect,"SELECT COUNT(login) FROM users");
-$photos=mysqli_query($connect,"SELECT COUNT(path) FROM images");
-//$active=mysqli_query($connect,"SELECT DISTINCT creator FROM images");
-$num=mysqli_fetch_assoc($query);
-//$a_num=mysqli_fetch_assoc($active);
-$p_num=mysqli_fetch_assoc($photos);
-//$num_p=mysqli_fetch_assoc($photos);
+$query=mysqli_query($connect,"SELECT login FROM users");
+$photos=mysqli_query($connect,"SELECT path FROM images");
+$registered=mysqli_num_rows($query);
+$uploaded=mysqli_num_rows($photos);
 $i=$s=$k=0;
-foreach ($num as $val){
-    $i+=$val;
-}
-foreach ($p_num as $val){
-    $s+=$val;
-}
-foreach ($a_num as $val){
-    $k+=$val;
-}
+$i= $registered;
+$s=$uploaded;
+
 
--- includes/delete.php
+++ includes/delete.php
@@ -4,8 +4,7 @@ if (isset($_GET['id'])){
     $id=$_GET['id'];
     $select=mysqli_query($connect,"SELECT path FROM images WHERE id='$id'");
     $row=mysqli_fetch_row($select);
-    $path =$row[0];
-    unlink('../'.$path);
+    unlink('../'.$row[0]);
     mysqli_query($connect,"DELETE FROM `favorites` WHERE imgd='$id'");
     $query="DELETE FROM `images` WHERE id=$id";
     mysqli_query($connect,$query);
